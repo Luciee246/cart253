eb701f2e2c944363d2961d0dd90e716bdf66c150
Lucie! <lsoussana@hotmail.com>
2025-09-09 15:17:02 -0400

    Created Instructions module
    - Created title, description, and background

---
+++ topics/instructions/js/script.js
@@ -0,0 +1,22 @@
+/**
+ * The Greatest Landscape
+ * Lucie Soussana, Yelena Arakelan, Scarlett Arriola
+ * 
+ * A beautiful landscape with land, a lake, a sun, a bird, a frog, and a house
+ */
+
+"use strict";
+
+/** Creates a blue canvas */
+function setup() {
+    createCanvas(950, 650);
+    background("#0492C2")
+}
+
+
+/**
+ * Draws land, a lake, a sun, a bird, a frog, and a house
+*/
+function draw() {
+
+}
