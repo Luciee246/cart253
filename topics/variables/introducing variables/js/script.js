4cbaddd0ae37da59cb62af8527ea367bc15b43ee
Lucie! <lsoussana@hotmail.com>
2025-09-15 13:56:51 -0400

    Started the introducing variables project

---
+++ topics/variables/introducing variables/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * introducing variables
+ * Lucie Soussana
+ * 
+ * Learning what a variable is and how to use it
+ */
+
+"use strict";
+
+/**
+ * Create  a canvas
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
