a354b5f31e34f006c5360473cc05ecbfbec68edf
Lucie! <lsoussana@hotmail.com>
2025-09-15 17:58:15 -0400

    Added glasses and organized functions
    Left to add:
    - Microphone that moves with cursor
    -Mouth that opens and closes with microphone movement

--- topics/art-jam/README.md
+++ topics/art-jam/README.md
@@ -6,8 +6,8 @@ Lucie Soussana
 
 ## Description
 
-This description should help the reader understand what the program is, anything they should know to be able to experience it (controls, special features, etc.), and what the desired user experience is. For example:
-
 > *My awesome self-portrait* is an interactive artwork that allows the user to move my favourite object, a microphone, closer to or farther from me.
 
 > The experience is controlled via the mouse. Bring the microphone closer to my mouth and I will prepare to sing, and vice-versa!
+
+I used the text function, which I never used, to write "Music is like oxygen to me"
--- topics/art-jam/js/script.js
+++ topics/art-jam/js/script.js
@@ -4,6 +4,9 @@
  * 
  * My face with a moveable microphone that I use to sing all the time
 
+*Uses:
+*p5.js
+*https://p5js.org/
 "use strict";
 
 /**
@@ -15,33 +18,88 @@ function setup() {
 }
 
 /**
- * Draws my face and a moveable microphone
+ * Draws a quote, my face and a moveable microphone
 */
 function draw() {
     background(255, 238, 140);
+
+    //Quote
+    drawQuote();
+
+    //My self-portait
+    drawHair();
+    drawFaceNeck();
+    drawEyes();
+    drawGlasses();
+    drawMouth();
+    drawClothes();
+}
+
+
+function drawQuote() {
+    //Quote
+    fill("#45503B");
+    textSize(25);
+    textAlign(RIGHT);
+    text("Music is like oxygen to me!", width / 2, 30);
+}
+
+function drawHair() {
     //Hair
+    push();
     noStroke();
     fill("#65350F");
     rect(200, 90, 240, 290, 60);
+    pop();
+}
 
-    //Face
+function drawFaceNeck() {
+    //Face and neck
+    push();
+    noStroke();
     fill("#F1D9B7");
     ellipse(320, 240, 200, 240);
-
-    //Neck
     rect(270, 330, 100, 80, 20);
+    pop();
+}
 
+function drawEyes() {
     //Eyes
+    push();
     fill("#2E1503");
     ellipse(280, 220, 30, 50);
     ellipse(360, 220, 30, 50);
+    pop();
+}
 
+function drawGlasses() {
+    //Glasses
+    push();
+    noFill();
+    stroke("#3D0734");
+    strokeWeight(5);
+    rect(230, 200, 80, 40, 10);
+    rect(330, 200, 80, 40, 10);
+    line(310, 220, 330, 220);
+    line(200, 200, 230, 220);
+    line(410, 220, 440, 200);
+    pop();
+}
+
+function drawMouth() {
     //Mouth
+    push();
     fill("#FF8886");
     noStroke();
     ellipse(320, 300, 50, 50);
+    pop();
+}
 
-    //Body
+function drawClothes() {
+    //Clothes
+    push();
+    noStroke();
     fill("#45503B");
     rect(170, 390, 300, 200, 50);
+    pop();
 }
