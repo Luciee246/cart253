4cfa34839da882083e8a4f240f7630c52f6066a2
Lucie! <lsoussana@hotmail.com>
2025-09-15 17:09:41 -0400

    Finished the base of me!
    Next to add:
    - Moving microphone that makes my mouth bigger or smaller

--- topics/art-jam/js/script.js
+++ topics/art-jam/js/script.js
@@ -14,10 +14,34 @@ function setup() {
 
 }
 
-
 /**
  * Draws my face and a moveable microphone
 */
 function draw() {
-    background(220);
+    background(255, 238, 140);
+    //Hair
+    noStroke();
+    fill("#65350F");
+    rect(200, 90, 240, 290, 60);
+
+    //Face
+    fill("#F1D9B7");
+    ellipse(320, 240, 200, 240);
+
+    //Neck
+    rect(270, 330, 100, 80, 20);
+
+    //Eyes
+    fill("#2E1503");
+    ellipse(280, 220, 30, 50);
+    ellipse(360, 220, 30, 50);
+
+    //Mouth
+    fill("#FF8886");
+    noStroke();
+    ellipse(320, 300, 50, 50);
+
+    //Body
+    fill("#45503B");
+    rect(170, 390, 300, 200, 50);
 }
