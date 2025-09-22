5a48ddfdc2c000371e9f84671a4517dac824f874
Lucie! <lsoussana@hotmail.com>
2025-09-22 14:39:05 -0400

    Debugged debugging variables!

--- topics/conditionals/debugging-conditionals/js/script.js
+++ topics/conditionals/debugging-conditionals/js/script.js
@@ -30,7 +30,7 @@ const bug = {
     },
     // Fear of the mouse: moving more than this many pixels in one frame
     // will scare the bug
-    mouseMoveThreshold: 3,
+    mouseMoveThreshold: 20,
     // Living or dead?
     alive: true
 };
@@ -73,20 +73,18 @@ function checkMouse() {
     // If the bug is scared it runs away fast!
     if (bugScared) {
         bug.velocity.y = 15; // Run away!
-
-
-        // Now check if the user managed to click on the bug
-        // First find out if the mouse if over the bug's body 
-        const mouseToBugDistance = dist(mouseX, mouseY, bug.x, bug.y);
-        const mouseOverBug = mouseToBugDistance < bug.w / 2;
-        // Check if the mouse is pressed over the bug
-        // (Not perfect since it would be better if the click happed right then
-        // but this will do)
-        if (mouseOverBug && mouseIsPressed) {
-            // Splat!
-            bug.alive = false;
-            bug.fill = bug.fills.dead;
-        }
+    }
+    // Now check if the user managed to click on the bug
+    // First find out if the mouse if over the bug's body 
+    const mouseToBugDistance = dist(mouseX, mouseY, bug.x, bug.y);
+    const mouseOverBug = mouseToBugDistance < bug.w / 2;
+    // Check if the mouse is pressed over the bug
+    // (Not perfect since it would be better if the click happed right then
+    // but this will do)
+    if (mouseOverBug && mouseIsPressed) {
+        // Splat!
+        bug.alive = false;
+        bug.fill = bug.fills.dead;
     }
 }
 
