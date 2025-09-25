5f367f79f9c7e65cac0a03d9c99448a70e7ad250
Lucie! <lsoussana@hotmail.com>
2025-09-25 16:33:19 -0400

    Added a keydown event handler for background changing

--- topics/events/plain-javascript-events/js/script.js
+++ topics/events/plain-javascript-events/js/script.js
@@ -7,17 +7,42 @@
 
 "use strict";
 
+// Information about the current and possible background fills
+const bg = {
+    fill: "#000000",
+    fills: {
+        black: "#000000",
+        white: "#ffffff"
+    },
+    switchKey: 32 // Space bar
+}
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates the canvas
 */
 function setup() {
+    createCanvas(400, 400);
 
+    // Listen for keypresses
+    window.addEventListener("keydown", changeBG);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Displays the background
 */
 function draw() {
+    background(bg.fill);
 
+}
+
+function changeBG(event) {
+    if (event.keyCode === bg.switchKey) {
+        if (bg.fill === bg.fills.black) {
+            bg.fill = bg.fills.white;
+        }
+        else {
+            bg.fill = bg.fills.black;
+        }
+    }
 }
