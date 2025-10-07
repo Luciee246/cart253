d12beb42f0eaadc3190978ef374cc23d95751660
Lucie! <lsoussana@hotmail.com>
2025-10-07 15:41:09 -0400

    Started brainstorminggg!

---
+++ topics/process/README.md
@@ -0,0 +1,9 @@
+# Brainstorminggg
+
+Lucie Soussana
+
+[View this project online](URL_FOR_THE_RUNNING_PROJECT)
+
+## Description
+
+> *Brainstorminggg* is where I write down all my weird ideas for possible games/works
---
+++ topics/process/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Brainstorminggg
+ * Lucie Soussana
+ * 
+ * Where my brain storms for ideas
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
