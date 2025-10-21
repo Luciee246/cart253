75d8a8f515d42d312ada964a14d9b1a42a359f5a
Lucie! <lsoussana@hotmail.com>
2025-10-21 00:11:54 -0400

    Created a create fly function

---
+++ topics/functions/buzzy-the-return-value/js/script.js
@@ -0,0 +1,68 @@
+/**
+ * Buzzy the return value
+ * Pippin Barr
+ * 
+ * Two flies that buzz around on the canvas
+ */
+
+"use strict";
+
+// Our flies that will buzz around
+let buzzyTheFly = undefined;
+let jazzyTheFly = undefined;
+let jacuzziTheFly = undefined;
+
+/**
+ * Create a canvas
+ */
+function setup() {
+    createCanvas(400, 400);
+
+    buzzyTheFly = createFly(10);
+    jazzyTheFly = createFly(2);
+    jacuzziTheFly = createFly(20);
+}
+
+function createFly(buzziness) {
+    let fly = {
+        x: random(100, width - 100),
+        y: random(100, height - 100),
+        size: 20,
+        buzziness: buzziness
+    };
+    return fly;
+}
+
+/**
+ * Background, move and draw buzzy
+ */
+function draw() {
+    background("#87ceeb");
+
+    moveFly(buzzyTheFly);
+    moveFly(jazzyTheFly);
+    moveFly(jacuzziTheFly);
+
+    drawFly(buzzyTheFly);
+    drawFly(jazzyTheFly);
+    drawFly(jacuzziTheFly);
+}
+
+/**
+ * Move the fly passed in by updating its position
+ */
+function moveFly(fly) {
+    fly.x += random(-fly.buzziness, fly.buzziness);
+    fly.y += random(-fly.buzziness, fly.buzziness);
+}
+
+/**
+ * Draw the fly passed in using its properties
+ */
+function drawFly(fly) {
+    push();
+    noStroke();
+    fill(0);
+    ellipse(fly.x, fly.y, fly.size);
+    pop();
+}
