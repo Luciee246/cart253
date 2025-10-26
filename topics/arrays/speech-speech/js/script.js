cf07cec5e6d98e6bd4cf2480bd34dda572bca96d
Lucie! <lsoussana@hotmail.com>
2025-10-26 13:09:33 -0400

    Finished speech speech

--- topics/arrays/speech-speech/js/script.js
+++ topics/arrays/speech-speech/js/script.js
@@ -7,17 +7,43 @@
 
 "use strict";
 
+// The speech itself
+const speech = ["Veni.", "Vidi.", "Vici.", "Sensi malum."];
+// Which sentence in the index to display
+let speechIndex = 0;
+
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Create a canvas
 */
 function setup() {
-
+    createCanvas(600, 100);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Display the current line of speech
 */
 function draw() {
+    background(0);
 
+    let currentLine = speech[speechIndex];
+
+    // Display the line
+    push();
+    fill(255);
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    text(currentLine, width / 2, height / 2);
+    pop();
+}
+
+function mousePressed() {
+    //Next line
+    speechIndex++;
+    //Wrap around if at the end
+    if (speechIndex >= speech.length) {
+        // Start over
+        speechIndex = 0;
+    }
 }
