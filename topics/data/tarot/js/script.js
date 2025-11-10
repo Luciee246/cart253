5632da54bae36a6a0f3ed06031f8e341ca27f997
Lucie! <lsoussana@hotmail.com>
2025-11-10 17:54:08 -0500

    Finished tarot project?
    The text bleeds out on the edge of the canvas, and I don't know how to fix that, which I am upset about, but it's fine.

--- topics/data/tarot/js/script.js
+++ topics/data/tarot/js/script.js
@@ -10,21 +10,38 @@
 // Our tarot data
 let tarot = undefined;
 
+// Our fortune
+let fortune = "Click to show a fortune.";
+
 function preload() {
     tarot = loadJSON("assets/data/tarot_interpretations.json");
 }
 
 /**
- * tbd.
+ * Create canvas
 */
 function setup() {
-
+    createCanvas(800, 400);
 }
 
-
 /**
- * tbd.
+ * Draw the tarot cards
 */
 function draw() {
+    background(0);
 
+    // Display the information
+    push();
+    textSize(16);
+    fill("yellow");
+    textAlign(CENTER, CENTER);
+    text(fortune, width / 2, height / 2);
+    pop();
+}
+
+function mousePressed() {
+    // Choose a random card
+    const card = random(tarot.tarot_interpretations);
+    // Choose a random fortune
+    fortune = random(card.fortune_telling);
 }
